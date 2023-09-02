a7b75ca80a204d04c3d2fb842b8b4ffde315bef4
Dylan To <dylan.to@hotmail.com>
2023-09-02 22:11:10 +0800

    Update env types

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,6 @@
 
 interface ImportMetaEnv {
   readonly CONTENTFUL_SPACE_ID: string;
-  readonly CONTENTFUL_DELIVERY_TOKEN: string;
-  readonly CONTENTFUL_PREVIEW_TOKEN: string;
+  readonly CONTENTFUL_ACCESS_TOKEN: string;
+  readonly CONTENTFUL_PREVIEW_ACCESS_TOKEN: string;
 }
