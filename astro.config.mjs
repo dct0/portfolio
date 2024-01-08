4417243d862d0dcfb4da1dd9e320009e11a0f4a9
Dylan To <dylan.to@hotmail.com>
2024-01-08 17:05:17 +0800

    Remove import, sitemap fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,25 @@
+import { defineConfig } from "astro/config";
+
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import { astroImageTools } from "astro-imagetools";
-import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
+import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   prefetch: true,
   adapter: vercel(),
-  integrations: [astroImageTools, react(), tailwind({
-    applyBaseStyles: false
-  }), sitemap(), icon()]
+  integrations: [
+    astroImageTools,
+    react(),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    sitemap(),
+    icon()
+  ],
+  site: "https://dct0.vercel.app"
 });
