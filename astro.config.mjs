ca3afc0f1ae867d1a786ff7e1a4ac7378dea7dc3
Dylan To <dylan.to@hotmail.com>
2024-01-08 16:58:05 +0800

    Remove simple-icons dep

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,14 @@ import vercel from "@astrojs/vercel/serverless";
 import { astroImageTools } from "astro-imagetools";
 import { defineConfig } from "astro/config";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   prefetch: true,
   adapter: vercel(),
-  integrations: [
-    astroImageTools,
-    react(),
-    tailwind({ applyBaseStyles: false }),
-    sitemap()
-  ]
+  integrations: [astroImageTools, react(), tailwind({
+    applyBaseStyles: false
+  }), sitemap(), icon()]
 });
